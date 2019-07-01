822da80726cdf8f51c9efb78fd89c766eefbc776
Ishmet <ishmetjahan@yahoo.com>
2019-07-01 01:21:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+
+class School 
+
+  attr_accessor :name, :grade
+  attr_reader :roster, :name 
+  
+  
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student (name,grade)
+    if @roster.has_key?(grade)
+    @roster[grade] << name
+  else
+  @roster[grade] = [name]
+  end
+  end 
+  
+  def grade (grade)
+    @roster[grade]
+  end
+  
+  def sort(name)
+    self.roster.each do |grade, name|
+    self.roster[grade] = name.sort
+  end
+  end
+  
+end 
+
+  
+
